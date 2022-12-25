d5454e867367779e802d8074eb5289181b11f2a6
awasevalentine <awasevalentine@gmail.com>
2022-12-26 00:34:32 +0100

    Created a service folder that will be responsible for fetching data from the server

---
+++ src/Services/getMovies.js
@@ -0,0 +1,24 @@
+import axios  from "axios"
+
+const ApiUrl = "http://www.omdbapi.com/?apikey=3b053d45&s=inception"
+const ApiUrlSearch = "http://www.omdbapi.com/?apikey=3b053d45&s="
+
+export const getMovies = async()=>{
+    try{
+        let getData = await axios.get(ApiUrl)
+
+        if(getData.data) return getData.data.Search
+    }catch(err){
+        throw new Error(`the following error occured: => ${err}`)
+    }
+}
+
+export const getAllMovies = async(search)=>{
+    try{
+        let getData = await axios.get(ApiUrlSearch + search)
+
+        if(getData.data) return getData.data.Search
+    }catch(err){
+        throw new Error(`the following error occured: => ${err}`)
+    }
+}
